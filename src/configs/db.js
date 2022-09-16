2abdcab2c6f320b0cb2205266c21fc52a91f8758
Ankit Thakur <95959359+thakurankit6767@users.noreply.github.com>
2022-09-17 00:42:51 +0530

    Add files via upload

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    return mongoose.connect("mongodb+srv://ankit:ankit12345@cluster0.pr05k.mongodb.net/lms")
+    return mongoose.connect("mongodb+srv://paragpatil187:Parag21@cluster0.0ljsu.mongodb.net/?retryWrites=true&w=majority")
 }
 
 module.exports = connect
